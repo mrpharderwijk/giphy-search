7c578b8adffe6e86cc69aa30954410c820c3108f
Your Name <you@example.com>
2019-10-30 20:11:26 +0100

    remove unused routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-// import { appRoutes } from './app.routing';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    // RouterModule.forRoot(appRoutes, { useHash: false, scrollPositionRestoration: 'top' }),
-    SharedModule,
-  ],
+  imports: [BrowserModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
